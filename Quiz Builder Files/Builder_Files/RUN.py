70e5419a10bba2beeb9962244f505f08630d67ea
Sajen Vasuthevan <vasuthe@uwindsor.ca>
2022-02-15 23:27:15 -0500

    Updated Version Function
    Updated the Version number

--- Quiz Builder Files/Builder_Files/RUN.py
+++ Quiz Builder Files/Builder_Files/RUN.py
@@ -1,9 +1,9 @@
-# Version 1.0.1
+# Version 1.0.2
 
 from Printer_File import Print_Same_Line
 
 def Version():
-    version = "Version 1.0.1"
+    version = "Version 1.0.2"
     sizeof_version = len(version)
     Print_Same_Line(sizeof_version, "*")
     print(version)
