03a6c33825e25c764a46f686865bd2f788bf48c9
Sajen Vasuthevan <vasuthe@uwindsor.ca>
2022-03-03 03:03:41 -0500

    View and Print File Temp Fix

--- Quiz Builder Files/Builder_Files/View.py
+++ Quiz Builder Files/Builder_Files/View.py
@@ -1,14 +1,38 @@
-from Directory_File import Print_FilesinDir
+from Directory_File import Check_SizeofDir, Print_FilesinDir
+from Printer_File import Print_Same_Line, Print_Same_New_Line, Print_String_Evenly_Same_Line
+
 list_ofQuiz = Print_FilesinDir()
 
-from Printer_File import Print_Same_Line, Print_String_Evenly_Same_Line
+len_longest_Quizname = -1
+key_longest_Quizname = -1
 
+def Check_Longest_Path():
+    len_temp = 0
+    for x in range(len(list_ofQuiz)):
+        if (len(list_ofQuiz[x]) > len_temp):
+            # print(list_ofQuiz[x])
+            # print(x)
+            len_temp = len(list_ofQuiz[x])
+            len_longest_Quizname = len_temp
+            print(len_temp)
+            # key_longest_Quizname = x
+
+
+Check_Longest_Path()
 title_ListofQuiz = "List of Quizzes"
-Print_Same_Line(len(max(list_ofQuiz, key=len)) + 3, "*")
-Print_String_Evenly_Same_Line(title_ListofQuiz, len(max(list_ofQuiz, key=len)) + 3, " ")
-Print_Same_Line(len(max(list_ofQuiz, key=len)) + 3, "*")
+len_QuizList = len(str(Check_SizeofDir()))
+final_length_size = len_longest_Quizname
+
+
+Print_Same_New_Line(final_length_size, "*")
+Print_String_Evenly_Same_Line(title_ListofQuiz, final_length_size, " ")
+Print_Same_New_Line(final_length_size, "*")
+    
+
 
 for x in range(len(list_ofQuiz)):
-    print(str(x+1) + ": " + str(list_ofQuiz[x]))
+    print(str(x+1) + ":", end = "")
+    Print_Same_Line((len_QuizList + 1) - len(str(x+1)), " ")
+    print(str(list_ofQuiz[x]))
 
-Print_Same_Line(len(max(list_ofQuiz, key=len)) + 3, "*")
+Print_Same_New_Line(final_length_size, "*")
